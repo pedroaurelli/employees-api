1570998e123f93096c83a8f02cec882d4f1962a4
pedroaurelli <pedroaureliano.contato@gmail.com>
2023-03-29 20:01:27 -0300

    Remove prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,10 +6,6 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
   root: true,
   env: {
     node: true,
@@ -21,5 +17,10 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'semi': ["error", "never"],
+    'comma-dangle': ["error", "never"],
+    'quotes': ["error", "single"],
+    'no-trailing-spaces': "error",
+    'no-multiple-empty-lines': "error",
   },
 };
