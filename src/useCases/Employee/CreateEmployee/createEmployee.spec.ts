6d465e7ba584e0465ee9a4dedaa79bca3b93e918
pedroaurelli <pedroaureliano.contato@gmail.com>
2023-04-01 13:41:38 -0300

    Add createEmployee test

---
+++ src/useCases/Employee/CreateEmployee/createEmployee.spec.ts
@@ -0,0 +1,37 @@
+import { Employee } from '@prisma/client'
+import { mock } from 'jest-mock-extended'
+import { PrismaService } from '../../../database/prisma.service'
+import { createEmployee } from './createEmployee'
+import { UpsertEmployeeParamsDTO } from '../../../model/UpsertEmployeeParamsDTO'
+
+describe('createEmployee', () => {
+  test('creates new employee', async () => {
+    const prisma = mock<PrismaService>({
+      employee: {
+        create: jest.fn()
+      }
+    })
+    const employee: UpsertEmployeeParamsDTO = {
+      name: 'john doe',
+      email: 'john.doe@email.test',
+      manager: null,
+      password: '123',
+      departament: 'T.I'
+    }
+
+    await createEmployee(prisma, employee)
+
+    expect(prisma.employee.create).toHaveBeenCalledWith(
+      expect.objectContaining({
+        data: {
+          email: employee.email,
+          createdAt: expect.any(Date),
+          password: expect.any(String),
+          id: expect.any(String),
+          departament: employee.departament,
+          manager: employee.manager,
+          name: employee.name
+        }
+      }))
+  })
+})
